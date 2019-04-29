32fe4c594cbe71f415bac39c4aa6077d9efe194d
Rafa Leblic <rleblic@gmail.com>
2019-04-29 17:52:31 +0200

    Prepare environment for poetry & pyenv

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,6 @@ set -x EDITOR vim
 # added by pipx (https://github.com/pipxproject/pipx)
 set -x PATH /Users/rleblic/.local/bin $PATH
 
+set -x PATH /Users/rleblic/.poetry/bin $PATH
+
+status --is-interactive; and source (pyenv init -|psub)
