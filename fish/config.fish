5a486684b06e69c3133892864a1e8aba9dd9d30c
Rafa Leblic <rleblic@gmail.com>
2019-05-22 20:11:06 +0200

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,6 @@ set -x PATH /Users/rleblic/.local/bin $PATH
 set -x PATH /Users/rleblic/.poetry/bin $PATH
 
 status --is-interactive; and source (pyenv init -|psub)
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+alias ls=lsd
