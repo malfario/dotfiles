46b6e9591a96e5ecaa4e7c6758577c64b5cfb0d4
Rafa Leblic <rleblic@gmail.com>
2020-12-11 12:19:05 +0100

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,11 @@ set -x PATH /Users/rleblic/.local/bin $PATH
 set -x PATH /Users/rleblic/.poetry/bin $PATH
 
 set -x PATH /Users/rleblic/bin $PATH
+set -x PATH /Users/rleblic/.nimble/bin $PATH
 
 set -x PATH /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin $PATH
+
+set -x TERM xterm-color
 
 status --is-interactive; and source (pyenv init -|psub)
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
