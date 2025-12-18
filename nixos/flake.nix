b96451c25bd2d5c05226174e64e784ac65be37d4
Rafa Leblic <rleblic@gmail.com>
2025-12-18 10:43:56 +0100

    Add NixOS config

---
+++ nixos/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
+  {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.rleblic.imports = [
+              ./home.nix
+            ];
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+              system = "x86_64-linux";
+            };
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ nixos/home.nix
@@ -0,0 +1,126 @@
+{ config, pkgs, inputs, ... }:
+{
+  imports = [
+    inputs.zen-browser.homeModules.beta
+  ];
+  
+  home.username = "rleblic";
+  home.homeDirectory = "/home/rleblic";
+  home.packages = with pkgs; [
+    scooter
+    grc
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "hx";
+    SHELL = "fish";
+  };
+
+  home.stateVersion = "25.11";
+  programs.home-manager.enable = true;
+
+  programs.zen-browser.enable = true;
+
+  programs.lazygit.enable = true;
+  programs.yazi.enable = true;
+
+  programs.fd.enable = true;
+  programs.fzf.enable = true;
+  programs.bat.enable = true;
+  programs.ripgrep.enable = true;
+  programs.direnv.enable = true;
+
+  programs.lsd = {
+    enableFishIntegration = true;
+    enable = true;
+  };
+
+  programs.carapace = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.fish = {
+    enable = true;
+    plugins = [
+      { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
+      { name = "hydro"; src = pkgs.fishPlugins.hydro.src; }
+      { name = "grc"; src = pkgs.fishPlugins.grc.src; }
+    ];
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.qutebrowser = {
+    enable = true;
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        decorations = "none";
+      };
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        email = "rleblic@gmail.com";
+        name = "Rafa Leblic";
+      };
+      alias = {
+        ci = "commit";
+        co = "checkout";
+        st = "status";
+        br = "branch";
+      };
+    };
+  };
+
+  programs.zellij = {
+    enable = true;
+  };
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "onedark";
+      editor = {
+        line-number = "relative";
+        cursor-shape = {
+          insert = "bar";
+          select = "underline";
+        };
+        indent-guides.render = true;
+        soft-wrap.enable = true;
+        bufferline = "multiple";
+        true-color = true;
+      };
+      keys.normal = {
+        space = {
+          b = {
+            d = ":buffer-close";
+            b = "buffer_picker";
+          };
+          f = {
+            f = "file_picker";
+            s = ":write";
+            t = ":toggle-option auto-format";
+          };
+          q = {
+            q = ":quit";
+          };
+          t = {
+            w = ":toggle whitespace.render all none";
+          };
+        };
+      };
+    };
+  };
+}
