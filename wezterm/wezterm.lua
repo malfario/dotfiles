6b927bde181ae6c9aa306dabae626a78d605f6f7
Rafa Leblic <rleblic@gmail.com>
2025-12-22 10:42:57 +0100

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,11 +12,22 @@ end
 
 -- This is where you actually apply your config choices
 
-config.color_scheme = 'OneDark (base16)'
-config.font = wezterm.font 'Hack NFM'
-config.font_size = 9.0
+-- config.initial_cols = 120
+-- config.initial_rows = 28
 
-config.default_prog = { 'powershell.exe' }
+-- config.color_scheme = 'OneDark (Gogh)'
+config.color_scheme = 'OneDark (base16)'
+
+config.font = wezterm.font 'Hack Nerd font'
+config.font_size = 12.0
+
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  config.default_prog = { 'powershell.exe' }
+end
+
+if wezterm.target_triple == 'aarch64-apple-darwin' then
+  -- config.window_decorations = "NONE | MACOS_FORCE_DISABLE_SHADOW"
+end
 
 -- and finally, return the configuration to wezterm
 return config
