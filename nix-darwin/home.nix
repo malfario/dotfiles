9601e1057bbbb4864c80e358974fe2d3c006849e
Rafa Leblic <rleblic@gmail.com>
2025-12-22 10:40:49 +0100

    Config kitty

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -8,6 +8,7 @@
 
   home.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
+    jetbrains-mono
     grc
   ];
 
@@ -23,20 +24,22 @@
       shellIntegration.enableFishIntegration = true;
       themeFile = "OneDark";
       font = {
-        name = "Menlo";
-        size = 12.0;
+        name = "JetBrains Mono";
+        size = 14.0;
       };
       settings = {
         macos_option_as_alt = "left";
         macos_show_window_title_in = "none";
         window_margin_width = 8.0;
         window_padding_width = 0.0;
-        hide_window_decorations = true;
+        hide_window_decorations = "titlebar-and-corners";
         macos_quit_when_last_window_closed = true;
         adjust_column_width = -1;
         cursor_trail = 100;
+        text_composition_strategy = "legacy";
       };
     };
+
     fish = {
       enable = true;
       plugins = [
