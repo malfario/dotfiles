55906b291441085efb32b91a512ba9dc92735a82
Rafa Leblic <rleblic@gmail.com>
2026-01-23 16:23:55 +0100

    Nix-darwin: home-manager config for atuin

--- nix-darwin/programs/atuin.nix
+++ nix-darwin/programs/atuin.nix
@@ -2,5 +2,12 @@
   programs.atuin = {
     enable = true;
     enableFishIntegration = true;
+    forceOverwriteSettings = true;
+    settings = {
+      filter_mode_shell_up_key_binding = "directory";
+      sync = {
+        records = true;
+      };
+    };
   };
 }
