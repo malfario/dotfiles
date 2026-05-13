d961245db8b6a638e88fd24343c049554410076c
Rafa Leblic <rleblic@gmail.com>
2026-05-13 13:37:18 +0200

    nix-darwin: update kitty

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -27,30 +27,10 @@
     ./programs/atuin.nix
     ./programs/aerospace.nix
     ./programs/zellij.nix
+    ./programs/kitty.nix
   ];
 
   programs = {
-    kitty = {
-      enable = true;
-      shellIntegration.enableFishIntegration = true;
-      themeFile = "OneDark";
-      font = {
-        name = "JetBrains Mono";
-        size = 14.0;
-      };
-      settings = {
-        macos_option_as_alt = "left";
-        macos_show_window_title_in = "none";
-        window_margin_width = 8.0;
-        window_padding_width = 0.0;
-        hide_window_decorations = "titlebar-and-corners";
-        macos_quit_when_last_window_closed = true;
-        adjust_column_width = -1;
-        cursor_trail = 100;
-        # text_composition_strategy = "legacy";
-      };
-    };
-
     fish = {
       enable = true;
       plugins = [
